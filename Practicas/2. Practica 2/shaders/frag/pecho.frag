8c9d3318e92acca07ba1a4eaed9a77da2851485d
Kevin Steve Quezada Ordoñez <steve10090@gmail.com>
2026-03-12 09:22:40 -0600

    feat[main.cpp, .vert, .frag]: agrega shaders de vértice y fragmento, reorganiza main para usar Window, ShaderProgram y Polygon

---
+++ Practicas/2. Practica 2/shaders/frag/pecho.frag
@@ -0,0 +1,8 @@
+// Amarillo #F4D03F
+#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(0.957f, 0.816f, 0.247f, 1.0f);
+}
--- Practicas/2. Practica 2/src/main.cpp
+++ Practicas/2. Practica 2/src/main.cpp
@@ -1,183 +1,127 @@
+// Define los vértices, crea los objetos y ejecuta el ciclo de renderizado
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <iostream>
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
-
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
+#include "Window.h"
+#include "ShaderProgram.h"
+#include "Polygon.h"
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    Window window(800, 600, "MyFirstOpenGL_Cat");
 
-// #ifdef __APPLE__
-//     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-// #endif
+    // Vértices del gato
 
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "MyFirstOpenGL_Dog", NULL, NULL);
-    if (window == NULL)
+    // Oreja Izquierda
+    float oreja_izq[] = {
+        -0.6542f,  0.9502f, 0.0f,
+        -0.6532f,  0.4826f, 0.0f,
+        -0.4303f,  0.7182f, 0.0f,
+    };
+
+    // Oreja Derecha
+    float oreja_der[] = {
+        -0.2062f,  0.9502f, 0.0f,
+        -0.4303f,  0.7182f, 0.0f,
+        -0.2061f,  0.4843f, 0.0f,
+    };
+
+    // Cabeza
+    float cabeza[] = {
+        -0.4303f,  0.7182f, 0.0f,
+        -0.2061f,  0.4843f, 0.0f,
+        -0.4274f,  0.2422f, 0.0f,
+        
+        -0.4303f,  0.7182f, 0.0f,
+        -0.4274f,  0.2422f, 0.0f,
+        -0.6532f,  0.4826f, 0.0f,
+    };
+
+    // Pecho
+    float pecho[] = {
+        -0.5855f,  0.3908f, 0.0f,
+        -0.5853f, -0.2692f, 0.0f,
+         0.0507f, -0.2647f, 0.0f,
+    };
+
+    // Cuerpo
+    float cuerpo[] = {
+        -0.5853f, -0.2692f, 0.0f,
+         0.0507f, -0.2647f, 0.0f,
+         0.0535f, -0.9347f, 0.0f,
+    };
+
+    // Pata
+    float pata[] = {
+        -0.2667f, -0.6061f, 0.0f,
+        -0.5842f, -0.9390f, 0.0f,
+         0.0535f, -0.9347f, 0.0f,
+    };
+
+    // Cola
+    float cola[] = {
+         0.0535f, -0.9347f, 0.0f,
+         0.3790f, -0.9393f, 0.0f,
+         0.6994f, -0.6050f, 0.0f,
+
+         0.0535f, -0.9347f, 0.0f,
+         0.6994f, -0.6050f, 0.0f,
+         0.3787f, -0.6050f, 0.0f,
+    };
+
+    // Crear objetos Polygon
+    Polygon polygon_oreja_izq(oreja_izq, 3);
+    Polygon polygon_oreja_der(oreja_der, 3);
+    Polygon polygon_cabeza(cabeza, 6);
+    Polygon polygon_pecho(pecho, 3);
+    Polygon polygon_cuerpo(cuerpo, 3);
+    Polygon polygon_pata(pata, 3);
+    Polygon polygon_cola(cola, 6);
+
+    // Crear objetos ShaderProgram
+    ShaderProgram shader_oreja_izq("shaders/vert/basic.vert", "shaders/frag/oreja_izq.frag");
+    ShaderProgram shader_oreja_der("shaders/vert/basic.vert", "shaders/frag/oreja_der.frag");
+    ShaderProgram shader_cabeza("shaders/vert/basic.vert", "shaders/frag/cabeza.frag");
+    ShaderProgram shader_pecho("shaders/vert/basic.vert", "shaders/frag/pecho.frag");
+    ShaderProgram shader_cuerpo("shaders/vert/basic.vert", "shaders/frag/cuerpo.frag");
+    ShaderProgram shader_pata("shaders/vert/basic.vert", "shaders/frag/pata.frag");
+    ShaderProgram shader_cola("shaders/vert/basic.vert", "shaders/frag/cola.frag");
+
+    // Ciclo de renderizado [main.cpp]
+    while (!window.shouldClose())
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+        if (glfwGetKey(window.handle, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window.handle, true);
 
-    if (glewInit() != GLEW_OK)
-    {
-        std::cerr << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    }
-
-    // build and compile our shader program
-    // ------------------------------------
-    // vertex shader
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // fragment shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // check for shader compile errors
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    // link shaders
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(fragmentShader);
-    glDeleteShader(vertexShader);
-
-
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left  
-         0.5f, -0.5f, 0.0f, // right 
-         0.0f,  0.5f, 0.0f  // top   
-    }; 
-
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0); 
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0); 
-
-
-    // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    // render loop
-    // -----------
-    while (!glfwWindowShouldClose(window))
-    {
-        // input
-        // -----
-        processInput(window);
-
-        // render
-        // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw our first triangle
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        // glBindVertexArray(0); // no need to unbind it every time 
- 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        // Dibujar cada parte del gato con su shader
+        shader_oreja_izq.use();
+        polygon_oreja_izq.draw();
+
+        shader_oreja_der.use();
+        polygon_oreja_der.draw();
+
+        shader_cabeza.use();
+        polygon_cabeza.draw();
+
+        shader_pecho.use();
+        polygon_pecho.draw();
+
+        shader_cuerpo.use();
+        polygon_cuerpo.draw();
+
+        shader_pata.use();
+        polygon_pata.draw();
+
+        shader_cola.use();
+        polygon_cola.draw();
+
+        window.swapBuffers();
+
+        window.pollEvents();
     }
 
-    // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
-
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
-    glfwTerminate();
     return 0;
-}
-
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
 }
